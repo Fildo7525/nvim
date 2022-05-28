3bacf96f3a1b4ff30c89e6e16e9274980079c94d
Filip Lobpreis <f.lobpreis@gmail.com>
2022-05-28 13:05:50 +0200

    keymaps: Reorganisation of the keymaps
    Sign off: Filip Lobpreis <f.lobpreis@gmail.com>

--- lua/usr/keymaps.lua
+++ lua/usr/keymaps.lua
@@ -29,11 +29,12 @@ keymap("n", "<F8>", ":split <bar> term ./build.sh<CR>", term_opts)
 keymap("n", "gf", ":e <cfile><CR>", opts)
 
 -- substitute spaces for tabs
-keymap("n", "<leader>ss", ":%s/    /<TAB>/g<CR>", opts)
+keymap("n", "<leader>4s", ":%s/    /<TAB>/g<CR>", opts)
+keymap("n", "<leader>2s", ":%s/  /<TAB>/g<CR>", opts)
 
 keymap("n", "<leader>lg", "<CMD>lua _LAZYGIT_TOGGLE()<CR>", opts)
 keymap("n", "<leader>ht", "<CMD>lua _HTOP_TOGGLE()<CR>", opts)
-keymap("n", "<leader>nt", "<CMD>lua _NCDU_TOGGLE()<CR>", opts)
+keymap("n", "<leader>nd", "<CMD>lua _NCDU_TOGGLE()<CR>", opts)
 
 -- TELESCOPE --
 keymap("n", "<leader>ff", "<cmd>Telescope find_files<CR>", opts)
@@ -65,7 +66,8 @@ keymap("n", "gdt", ":lua getCommitIndex()<CR>", opts)
 
 -- SAVING --
 keymap("n", "<leader>w", ":w<CR>", opts)
-keymap("n", "<leader>qq", ":wq<CR>", opts)
+keymap("n", "<leader>wq", ":wq<CR>", opts)
+keymap("n", "<leader>qq", ":q<CR>", opts)
 keymap("n", "<leader>bw", ":w <bar> :Bdelete! %d <CR>", opts) -- buffer writing
 keymap("n", "<leader>bd", ":Bdelete! %d<CR>", opts) -- buffer delete force
 
@@ -76,36 +78,28 @@ keymap("n", "<C-LEFT>", ":vertical resize -2<CR>", opts)
 keymap("n", "<C-RIGHT>", ":vertical resize +2<CR>", opts)
 
 --	NAVIGATE WINDOWS	--
-keymap("n", "<M-C-k>", "<C-w>k", opts)
-keymap("n", "<M-C-j>", "<C-w>j", opts)
+keymap("n", "<C-k>", "<C-w>k", opts)
+keymap("n", "<C-j>", "<C-w>j", opts)
 keymap("n", "<C-h>", "<C-w>h", opts)
 keymap("n", "<C-l>", "<C-w>l", opts)
 
 -- keymap("n", "<leader>e", ":Lex 15<CR>", opts)
 
 -- NAVIGATE BUFFERS --
-keymap("n", "<A-,>", ":bprevious<CR>", opts)
-keymap("n", "<A-.>", ":bnext<CR>", opts)
+keymap("n", "<S-h>", ":bprevious<CR>", opts)
+keymap("n", "<S-l>", ":bnext<CR>", opts)
 
 -- MOVE TEXT UP & DOWN
 -- keymap("n", "<A-S-k>", "<ESC>:m .+1<CR>==gi", opts)
 -- keymap("n", "<A-S-j>", "<ESC>:m .-2<CR>==gi", opts)
-keymap("n", "<A-S-j>", ":move '<-2<CR>", opts)
-keymap("n", "<A-S-k>", ":move '>+1<CR>", opts)
+keymap("n", "<C-S-j>", ":move '<-2<CR>", opts)
+keymap("n", "<C-S-k>", ":move '>+1<CR>", opts)
 
 -- NEOGEN
 keymap("n", "<leader>nc", ":lua require('neogen').generate({ type = 'class'})<CR>", opts)
 keymap("n", "<leader>nf", ":lua require('neogen').generate({ type = 'func'})<CR>", opts)
 keymap("n", "<leader>nt", ":lua require('neogen').generate({ type = 'type'})<CR>", opts)
 keymap("n", "<leader>ni", ":lua require('neogen').generate({ type = 'file'})<CR>", opts)
-
--- Insert --
-
--- BRACES, PARANTHESES
--- keymap("i", "[", "[]<LEFT>", opts)
--- keymap("i", "{", "{}<LEFT>", opts)
--- keymap("i", "'", "''<LEFT>", opts)
--- keymap("i", "\"", "\"\"<LEFT>", opts)
 
 -- Press jk fast to enter
 keymap("i", "jj", "<ESC>:w<CR>", opts)
@@ -127,16 +121,6 @@ keymap("v", "p", '"_dP', opts)
 -- Move text up and down
 keymap("x", "J", ":move '>+1<CR>gv-gv", opts)
 keymap("x", "K", ":move '<-2<CR>gv-gv", opts)
-keymap("x", "<A-S-DOWN>", ":move '>+1<CR>gv-gv", opts)
-keymap("x", "<A-S-UP>", ":move '<-2<CR>gv-gv", opts)
-
--- Terminal --
-
--- Better terminal navigation
-keymap("t", "<C-h>", "<C-\\><C-N><C-w>h", term_opts)
-keymap("t", "<C-j>", "<C-\\><C-N><C-w>j", term_opts)
-keymap("t", "<C-k>", "<C-\\><C-N><C-w>k", term_opts)
-keymap("t", "<C-l>", "<C-\\><C-N><C-w>l", term_opts)
 
 -- Nvimtree
 keymap("n", "<leader>e", ":NvimTreeToggle<cr>", opts)
