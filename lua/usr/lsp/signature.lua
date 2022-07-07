132c5ae989350de7b4ce5928818b2e4ae78ea499
Filip Lobpreis <f.lobpreis@gmail.com>
2022-07-07 21:22:50 +0200

    signature: Fixed floating window covering current text
    Signed-off-by: Filip Lobpreis <f.lobpreis@gmail.com>

--- lua/usr/lsp/signature.lua
+++ lua/usr/lsp/signature.lua
@@ -18,21 +18,21 @@ local config = {
 
 	floating_window = true, -- show hint in a floating window, set to false for virtual text only mode
 
-	floating_window_above_cur_line = true, -- try to place the floating above the current line when possible Note:
-	-- will set to true when fully tested, set to false will use whichever side has more space
-	-- this setting will be helpful if you do not want the PUM and floating win overlap
+	floating_window_above_cur_line = true, -- try to place the floating above the current line when possible
+	-- NOTE: Will set to true when fully tested, set to false will use whichever side has more space
+	-- NOTE: this setting will be helpful if you do not want the PUM and floating win overlap
 
- 	floating_window_off_x = 1, -- adjust float windows x position.
- 	floating_window_off_y = 5, -- adjust float windows y position.
+	floating_window_off_x = 1, -- adjust float windows x position.
+	floating_window_off_y = -1, -- adjust float windows y position.
 
 	fix_pos = false,  -- set to true, the floating window will not auto-close until finish all parameters
-	hint_enable = false, -- virtual hint enable
+	hint_enable = true, -- virtual hint enable
 	hint_prefix = "🐼 ",  -- Panda for parameter
 	hint_scheme = "String",
 	hi_parameter = "LspSignatureActiveParameter", -- how your parameter will be highlight
-	max_height = 12, -- max height of signature floating_window, if content is more than max_height, you can scroll down
+	max_height = 5, -- max height of signature floating_window, if content is more than max_height, you can scroll down
 				   -- to view the hiding contents
-	max_width = 80, -- max_width of signature floating_window, line will be wrapped if exceed max_width
+	max_width = 100, -- max_width of signature floating_window, line will be wrapped if exceed max_width
 	handler_opts = {
 		border = "rounded"   -- double, rounded, single, shadow, none
 	},
