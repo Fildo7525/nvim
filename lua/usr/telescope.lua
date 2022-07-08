ded5011b715d0fcfbf5a2aacb1647f566a8fbd5c
Filip Lobpreis <f.lobpreis@gmail.com>
2022-07-08 18:34:51 +0200

    telescope: Updating telescope visualisation
    Telescope will visualise as vertical. Path will be displayed as full
    until it will not fit. Than the beggining of the path will be truncated.
    
    Signed-off-by: Filip Lobpreis <f.lobpreis@gmail.com>

--- lua/usr/telescope.lua
+++ lua/usr/telescope.lua
@@ -10,10 +10,29 @@ local actions = require("telescope.actions")
 
 telescope.setup {
 	defaults = {
+		layout_strategy = 'vertical',
+		-- horizontal (default)
+		-- vertical
+		-- cursor
+		-- bottom_pane
+		-- center
 
 		prompt_prefix = " ",
 		selection_caret = " ",
-		path_display = { "smart" },
+		path_display = { "truncate" },
+		 -- path_display can be set to an array with a combination of:
+			-- - "hidden"	hide file names
+			-- - "tail"	  only display the file name, and not the path
+			-- - "absolute"  display absolute paths
+			-- - "smart"	 remove as much from the path as possible to only show
+			--			   the difference between the displayed paths.
+			--			   Warning: The nature of the algorithm might have a negative
+			--			   performance impact!
+			-- - "shorten"   only display the first character of each directory in
+			--			   the path
+			-- - "truncate"  truncates the start of the path when the whole path will
+			--			   not fit. To increase the the gap between the path and the edge.
+			--			   set truncate to number `truncate = 3`
 
 		mappings = {
 			i = {
