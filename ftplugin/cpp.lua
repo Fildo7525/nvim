3d341681f72bd8a94f086d0142f1b44966ef32e7
Filip Lobpreis <f.lobpreis@gmail.com>
2022-07-12 13:15:44 +0200

    cpp: update of filetype file
    Signed-off-by: Filip Lobpreis <f.lobpreis@gmail.com>

--- ftplugin/cpp.lua
+++ ftplugin/cpp.lua
@@ -5,9 +5,9 @@ local opts = { noremap = true, silent = true }
 local keymap = vim.api.nvim_set_keymap
 
 function QtQuerryFinder()
-	address = "https://doc.qt.io/qt-5/" .. vim.fn.expand("<cword>"):lower() .. ".html"
+	local address = "https://doc.qt.io/qt-5/" .. vim.fn.expand("<cword>"):lower() .. ".html"
 	vim.cmd(":!xdg-open " .. address)
 end
 
 keymap("n", "<leader>sh", ":ClangdSwitchSourceHeader<CR>", opts)
-keymap("n", "<F2>", ":lua QtQuerryFinder()<CR>", opts)
+keymap("n", "<leader>qf", ":lua QtQuerryFinder()<CR>", opts)
