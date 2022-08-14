d1bccc11084370eb23b190c7f8f079952be82370
Filip Lobpreis <f.lobpreis@gmail.com>
2022-08-14 19:39:48 +0200

    cpp: Open url address in web browser

--- ftplugin/cpp.lua
+++ ftplugin/cpp.lua
@@ -6,7 +6,7 @@ local keymap = vim.api.nvim_set_keymap
 
 function QtQuerryFinder()
 	local address = "https://doc.qt.io/qt-5/" .. vim.fn.expand("<cword>"):lower() .. ".html"
-	vim.cmd(":!xdg-open " .. address)
+	vim.cmd(":!kfmclient openURL " .. address)
 end
 
 keymap("n", "<leader>sh", ":ClangdSwitchSourceHeader<CR>", opts)
