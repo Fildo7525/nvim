6e63c1dfe09188fc42b609a114a6d991d5e391bc
Filip Lobpreis <f.lobpreis@gmail.com>
2022-08-16 00:52:21 +0200

    cpp: function definition is more general

--- ftplugin/cpp.lua
+++ ftplugin/cpp.lua
@@ -4,6 +4,10 @@ local opts = { noremap = true, silent = true }
 
 local keymap = vim.api.nvim_set_keymap
 
+function Trim(s)
+  return (string.gsub(s, "^%s*(.-)%s*$", "%1"))
+end
+
 function QtQuerryFinder()
 	local address = "https://doc.qt.io/qt-5/" .. vim.fn.expand("<cword>"):lower() .. ".html"
 	vim.cmd(":!kfmclient openURL " .. address)
@@ -15,33 +19,30 @@ end
 -- 		e.g.: WRONG: QMap<QString, QString> functinoName();
 -- 								  ^
 -- 		The space is the problem.
--- 	3) The function must be in a class.
--- 	4) The class must have the same name as the class.
-function CreateFunctionDefinition()
-	local line = vim.fn.getline('.')
-	local file = vim.api.nvim_buf_get_name(0)
-
-	local indexOfLastSlashInName = string.find(file, "/[^/]*$")
-
-	local className = string.sub(file, indexOfLastSlashInName)
-	className = string.sub(className, 2)
-
+-- 	3) The functino must be defined on one line.
+--
+---@param file           string - Is a header file name with a full path.
+-- 				  The file can have extension either .h or .hpp.
+---@param line           string - Full line taken from the edditor. The line must end with a semicolon.
+--				  The function must be defined in one line.
+---@param className?     string - If the function is not in a class give there a nil.
+function CreateDefinition(file, line, className)
 	if string.sub(file, -1) == "h" then
 		file = string.gsub(file, "[.]%w$","")
-		className = string.gsub(className, "[.]%w$","")
 	else
 		file = string.gsub(file, "[.]%w%w%w$", "")
-		className = string.gsub(className, "[.]%w%w%w$", "")
 	end
-
-	line = string.sub(line, 2, -2)
-	line = line .. "\n{\n\n}"
-	line = string.gsub(line, " ", " " .. className .. "::", 1)
-
 	file = file .. ".cpp"
 
-	local out = io.open(file, 'a+')
+	line = Trim(line)
+	line = string.sub(line, 1, -2)
+	line = line .. "\n{\n\n}"
+	if className ~= nil then
+		line = string.gsub(line, " ", " " .. className .. "::", 1)
+		print(line)
+	end
 
+	local out = io.open(file, 'a+')
 	if out ~= nil then
 		out:write(line, "\n\n")
 		out:flush()
@@ -49,7 +50,32 @@ function CreateFunctionDefinition()
 	end
 end
 
+function CreateClassMethodDefinition()
+	local line = vim.fn.getline('.')
+	local file = vim.api.nvim_buf_get_name(0)
+
+	local indexOfLastSlashInName = string.find(file, "/[^/]*$")
+
+	local className = string.sub(file, indexOfLastSlashInName)
+	className = string.sub(className, 2)
+	if string.sub(file, -1) == "h" then
+		className = string.gsub(className, "[.]%w$","")
+	else
+		className = string.gsub(className, "[.]%w%w%w$", "")
+	end
+
+	CreateDefinition(file, line, className)
+end
+
+function CreateFunctionDefinition()
+	local line = vim.fn.getline('.')
+	local file = vim.api.nvim_buf_get_name(0)
+
+	CreateDefinition(file, line, nil)
+end
+
 keymap("n", "<leader>sh", ":ClangdSwitchSourceHeader<CR>", opts)
 keymap("n", "<F2>", ":lua QtQuerryFinder()<CR>", opts)
-keymap("n", "<F3>", ":lua CreateFunctionDefinition()<CR>", opts)
+keymap("n", "<F3>", ":lua CreateClassMethodDefinition()<CR>", opts)
+keymap("n", "<F4>", ":lua CreateFunctionDefinition()<CR>", opts)
 
