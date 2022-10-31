a76b5de37bceb7acf75d7cdba5c58a245fb963d4
Filip Lobpreis <f.lobpreis@gmail.com>
2022-10-31 23:36:04 +0100

    catppuccin: Default config update
    Signed-off-by: Filip Lobpreis <f.lobpreis@gmail.com>

--- lua/usr/core/colorscheme/configs/cattpucin.lua
+++ lua/usr/core/colorscheme/configs/cattpucin.lua
@@ -4,12 +4,14 @@ local colors = require("catppuccin.palettes").get_palette()
 local scheme = require(colorscheme)
 
 scheme.setup({
+	flavour = "mocha", -- latte, frappe, macchiato, mocha
+	background = { -- :h background
+		light = "latte",
+		dark = "mocha",
+	},
+	compile_path = vim.fn.stdpath("cache") .. "/catppuccin",
 	transparent_background = false,
 	term_colors = false,
-	compile = {
-		enabled = false,
-		path = vim.fn.stdpath("cache") .. "/catppuccin",
-	},
 	dim_inactive = {
 		enabled = false,
 		shade = "dark",
@@ -29,80 +31,27 @@ scheme.setup({
 		types = {},
 		operators = {},
 	},
-	integrations = {
-		aerial = false,
-		barbar = false,
-		beacon = false,
-		cmp = true,
-		coc_nvim = false,
-		dashboard = true,
-		fern = false,
-		fidget = false,
-		gitgutter = false,
-		gitsigns = true,
-		hop = false,
-		illuminate = false,
-		leap = false,
-		lightspeed = false,
-		lsp_saga = false,
-		lsp_trouble = false,
-		markdown = true,
-		mini = false,
-		neogit = false,
-		neotest = false,
-		neotree = false,
-		notify = true,
-		nvimtree = true,
-		overseer = false,
-		pounce = false,
-		symbols_outline = false,
-		telekasten = false,
-		telescope = true,
-		treesitter = true,
-		treesitter_context = true,
-		ts_rainbow = false,
-		vim_sneak = false,
-		vimwiki = false,
-		which_key = false,
-
-		-- Special integrations, see https://github.com/catppuccin/nvim#special-integrations
-		dap = {
-			enabled = false,
-			enable_ui = false,
-		},
-		indent_blankline = {
-			enabled = true,
-			colored_indent_levels = false,
-		},
-		native_lsp = {
-			enabled = true,
-			virtual_text = {
-				errors = { "italic" },
-				hints = { "italic" },
-				warnings = { "italic" },
-				information = { "italic" },
-			},
-			underlines = {
-				errors = { "underline" },
-				hints = { "underline" },
-				warnings = { "underline" },
-				information = { "underline" },
-			},
-		},
-		navic = {
-			enabled = false,
-			custom_bg = "NONE",
-		},
-	},
 	color_overrides = {},
+	custom_highlights = {},
 	highlight_overrides = {
 		mocha = {
 			Comment = { fg = "#ADADAD" },
 		},
 	},
+	integrations = {
+		cmp = true,
+		gitsigns = true,
+		nvimtree = true,
+		telescope = true,
+		treesitter = true,
+		integration = {
+			dap = {
+				enabled = true,
+				enable_ui = true, -- enable nvim-dap-ui
+			},
+		}
+		-- For more plugins integrations please scroll down (https://github.com/catppuccin/nvim#integrations)
+	},
 })
-
-vim.g.catppuccin_flavour = "mocha" -- latte, frappe, macchiato 
-
 return colorscheme
 
