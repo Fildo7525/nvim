a78f4e4ab157a7259e58f5c2a5b6f8a8653ad962
Filip Lobpreis <f.lobpreis@gmail.com>
2022-11-18 21:36:18 +0100

    dap: generalize home directory
    Signed-off-by: Filip Lobpreis <f.lobpreis@gmail.com>

--- lua/usr/DAP/settings/dapcpp.lua
+++ lua/usr/DAP/settings/dapcpp.lua
@@ -1,11 +1,12 @@
 local dap = require('dap')
 
+local homePath = vim.fn.expand('$HOME')
 
 dap.adapters.cppdbg = {
 	id = 'cppdbg',
 	type = 'executable',
 	-- Must be from vscode cannot be cloned DUNNO why
-	command = '/home/fildo7525/.vscode/extensions/ms-vscode.cpptools-1.12.4-linux-x64/debugAdapters/bin/OpenDebugAD7',
+	command = homePath .. '/.vscode/extensions/ms-vscode.cpptools-1.12.4-linux-x64/debugAdapters/bin/OpenDebugAD7',
 }
 
 dap.configurations.cpp = {
