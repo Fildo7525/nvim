2e2b939e94711659e5f58e9036233454bbefcae0
Filip Lobpreis <f.lobpreis@gmail.com>
2022-12-18 12:30:13 +0100

    dap: keymaps switch
    Signed-off-by: Filip Lobpreis <f.lobpreis@gmail.com>

--- lua/usr/DAP/nvimdap.lua
+++ lua/usr/DAP/nvimdap.lua
@@ -8,9 +8,9 @@ vim.fn.sign_define('DapBreakpoint', {text='🛑', texthl='', linehl='', numhl=''
 vim.fn.sign_define('DapStopped', {text='',  texthl='', linehl='', numhl=''})
 
 keymap("n", "<leader>dc", ":lua require'dap'.continue()<CR>", opts)
-keymap("n", "<F10>", ":lua require'dap'.step_over()<CR>", opts)
-keymap("n", "<F11>", ":lua require'dap'.step_into()<CR>", opts)
-keymap("n", "<S-F11>", ":lua require'dap'.step_out()<CR>", opts)
+keymap("n", "<F11>", ":lua require'dap'.step_over()<CR>", opts)
+keymap("n", "<F12>", ":lua require'dap'.step_into()<CR>", opts)
+keymap("n", "<S-F6>", ":lua require'dap'.step_out()<CR>", opts)
 keymap("n", "<leader>br", ":lua require'dap'.toggle_breakpoint()<CR>", opts)
 keymap("n", "<leader>tb", ":lua require'dapui'.toggle()<CR>", opts)
 keymap("n", "<leader>cb", ":lua require'dap'.set_breakpoint(vim.fn.input('Breakpoint condition: '))<CR>", opts)
