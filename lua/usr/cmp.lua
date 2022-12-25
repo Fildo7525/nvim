9f8df15ce154b9321e6ba04dca98b217529f3533
Filip Lobpreis <f.lobpreis@gmail.com>
2022-12-25 15:54:47 +0100

    cmp: unlink luasnip from tab
    Signed-off-by: Filip Lobpreis <f.lobpreis@gmail.com>

--- lua/usr/cmp.lua
+++ lua/usr/cmp.lua
@@ -33,10 +33,6 @@ cmp.setup {
 		["<Tab>"] = cmp.mapping(function(fallback)
 			if cmp.visible() then
 				cmp.select_next_item()
-			elseif luasnip.expandable() then
-				luasnip.expand()
-			--[[ elseif luasnip.expand_or_jumpable() then ]]
-			--[[ 	luasnip.expand_or_jump() ]]
 			elseif check_backspace() then
 				fallback()
 			elseif require('neogen').jumpable() then
