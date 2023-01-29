f642d824ce29eccc4aaef12d45cf90ae56b4eade
Filip Lobpreis <f.lobpreis@gmail.com>
2023-01-29 12:01:11 +0100

    project: update project patterns
    Signed-off-by: Filip Lobpreis <f.lobpreis@gmail.com>

--- lua/usr/project.lua
+++ lua/usr/project.lua
@@ -14,17 +14,17 @@ project.setup({
 	-- All the patterns used to detect root dir, when **"pattern"** is in
 	-- detection_methods
 	patterns = {
+		"build/",
+		"log/",
 		".git",
 		"_darcs",
 		".hg",
 		".bzr",
 		".svn",
-		".Makefile",
 		".package.json",
 		".pom.xml",
 		".build.sh",
 		".configure.sh",
-		"compile_commands.json",
 	},
 
 	-- Table of lsp clients to ignore by name
