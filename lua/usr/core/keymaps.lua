64e99e548981ac9632837f3f19be1a91720edfea
Filip Lobpreis <f.lobpreis@gmail.com>
2023-02-03 20:32:05 +0100

    keymap: renamed buildProject scripts
    Signed-off-by: Filip Lobpreis <f.lobpreis@gmail.com>

--- lua/usr/core/keymaps.lua
+++ lua/usr/core/keymaps.lua
@@ -41,8 +41,8 @@ keymap("n", "<leader>sc", ":e ~/.config/nvim/init.lua<CR>", opts)
 -- buildProject CUSTOMS --
 keymap("n", "<F2>", ":term cmake -S . -B ./build && mv ./build/compile_commands.json .<CR>", opts)
 keymap("n", "<F4>", ":set spell!<CR>", term_opts)
-keymap("n", "<F5>", ":term ./compile.sh<CR>", term_opts)
-keymap("n", "<F6>", ":term ./build.sh<CR>", term_opts)
+keymap("n", "<F5>", ":term ./compile<CR>", term_opts)
+keymap("n", "<F6>", ":term ./run<CR>", term_opts)
 keymap("n", "<C-F8>", ":split <bar> term ./build.sh<CR>", term_opts)
 
 keymap("n", "<leader>vs", ":split<CR>", opts)
