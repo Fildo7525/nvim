04f205a41757ffd0f7dd97c69b848d48c2ef071b
Filip Lobpreis <f.lobpreis@gmail.com>
2023-02-17 21:46:50 +0100

    clangd: new options for clangd startup
    Signed-off-by: Filip Lobpreis <f.lobpreis@gmail.com>

--- lua/usr/lsp/settings/clangd.lua
+++ lua/usr/lsp/settings/clangd.lua
@@ -10,11 +10,14 @@ local root_files = {
 	'compile_flags.txt',
 	'build.sh', -- buildProject
 	'configure.ac', -- AutoTools
+	'run',
+	'compile',
 }
 
 -- TODO: add clang-tidy to on_atach with clangd
 return {
 	cmd = { "clangd",
+		"--all-scopes-completion",
 		"--background-index",
 		"--clang-tidy",
 		"--compile_args_from=filesystem", -- lsp-> does not come from compie_commands.json
@@ -22,11 +25,17 @@ return {
 		"--completion-style=bundled",
 		"--cross-file-rename",
 		"--debug-origin",
+		"--enable-config", -- clangd 11+ supports reading from .clangd configuration file
+		"--fallback-style=Qt",
+		"--folding-ranges",
 		"--function-arg-placeholders",
 		"--header-insertion=iwyu",
+		"--pch-storage=memory", -- could also be disk
+		"--suggest-missing-includes",
 		"-j=4",		-- number of workers
 		-- "--resource-dir="
-		"--suggest-missing-includes",
+		"--log=error",
+		--[[ "--query-driver=/usr/bin/g++", ]]
 	},
 	filetypes = { "c", "cpp", "objc", "objcpp" },
 	root_dir = function(fname)
