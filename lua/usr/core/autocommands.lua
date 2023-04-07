b07796684de0d28fdac3b8602b64b1e31c2cbf02
Filip Lobpreis <f.lobpreis@gmail.com>
2023-04-07 23:11:57 +0200

    core: remove unused autocommands
    Signed-off-by: Filip Lobpreis <f.lobpreis@gmail.com>

--- lua/usr/core/autocommands.lua
+++
@@ -1,7 +0,0 @@
-
-vim.cmd [[
-	augroup reload_cmake
-		autocmd!
-		autocmd BufWritePost CMakeLists.txt :! cmake -S . -B ./build && cp ./build/compile_commands.json .
-	augroup end
-]]
--- lua/usr/core/init.lua
+++ lua/usr/core/init.lua
@@ -1,4 +1,3 @@
 require("usr.core.colorscheme")
 require("usr.core.options")
 require("usr.core.keymaps")
---[[ require("usr.core.autocommands") ]]
