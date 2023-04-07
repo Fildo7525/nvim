9d17d8609f3e4851a25ed30f3caa17672e7baddf
Filip Lobpreis <f.lobpreis@gmail.com>
2023-04-07 16:46:10 +0200

    treesitter: autoinstall missing  parsers
    Signed-off-by: Filip Lobpreis <f.lobpreis@gmail.com>

--- lua/usr/ts/treesitter.lua
+++ lua/usr/ts/treesitter.lua
@@ -8,6 +8,10 @@ treesitter.setup {
 	-- Install languages synchronously (only applied to `ensure_installed`)
 	sync_install = false,
 
+	-- Automatically install missing parsers when entering buffer
+	-- Recommendation: set to false if you don't have `tree-sitter` CLI installed locally
+	auto_install = true,
+
 	-- List of parsers to ignore installing
 	ignore_install = { "" },
 
