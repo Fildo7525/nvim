4da92bf5542a12797840d5abee132d32aa492d7b
Filip Lobpreis <f.lobpreis@gmail.com>
2023-04-23 14:31:29 +0200

    ftplugin: tex: macro for ~ instead of spaces
    Signed-off-by: Filip Lobpreis <f.lobpreis@gmail.com>

--- ftplugin/tex.lua
+++ ftplugin/tex.lua
@@ -1,1 +1,42 @@
 vim.opt.textwidth=120
+
+local no_at_end = {
+	"a",
+	"aj",
+	"ak",
+	"ale",
+	"cez",
+	"do",
+	"k",
+	"kde",
+	"ku",
+	"na",
+	"o",
+	"od",
+	"po",
+	"pre",
+	"preto",
+	"pri",
+	"s",
+	"so",
+	"sú",
+	"tak",
+	"to",
+	"u",
+	"už",
+	"v",
+	"vo",
+	"z",
+	"za",
+	"zo",
+	"že",
+	"či",
+}
+
+vim.keymap.set('n', 'lqf', function ()
+	for _, x in pairs(no_at_end) do
+		vim.cmd('%s/\\<' .. x .. ' /' .. x .. '\\~/ge')
+		x = x:gsub("(%l)(%w*)", function(a,b) return string.upper(a)..b end) 
+		vim.cmd('%s/\\<' .. x .. ' /' .. x .. '\\~/ge')
+	end
+end)
