cfa971fa5762257c280a7124914fd98a0f8a924b
Filip Lobpreis <f.lobpreis@gmail.com>
2023-05-08 12:21:26 +0200

    cmp: update experimental ghost text
    Signed-off-by: Filip Lobpreis <f.lobpreis@gmail.com>

--- lua/usr/cmp.lua
+++ lua/usr/cmp.lua
@@ -13,8 +13,6 @@ cmp.setup {
 		end,
 	},
 	mapping = {
-		["<C-k>"] = cmp.mapping.select_prev_item(),
-		["<C-j>"] = cmp.mapping.select_next_item(),
 		["<C-b>"] = cmp.mapping(cmp.mapping.scroll_docs(-1), { "i", "c" }),
 		["<C-f>"] = cmp.mapping(cmp.mapping.scroll_docs(1), { "i", "c" }),
 		["<C-Space>"] = cmp.mapping(cmp.mapping.complete(), { "i", "c" }),
@@ -86,7 +84,9 @@ cmp.setup {
 		documentation = cmp.config.window.bordered(),
 	},
 	experimental = {
-		ghost_text = true,
+		ghost_text ={
+			hl_group = "Comment",
+		},
 		native_menu = false,
 	},
 	sorting = {
