9f53c4c708d34c9b05e41d192280ae05b4e28029
Filip Lobpreis <f.lobpreis@gmail.com>
2023-05-31 18:18:12 +0200

    lazy: inlay hints
    Signed-off-by: Filip Lobpreis <f.lobpreis@gmail.com>

--- lua/usr/lsp/init.lua
+++ lua/usr/lsp/init.lua
@@ -6,5 +6,6 @@ require("usr.lsp.handlers").setup()
 require("usr.lsp.semantic")
 require("usr.lsp.signature")
 require("usr.lsp.null_ls")
+require("usr.lsp.codelens")
 vim.cmd("source " .. os.getenv("HOME") .. "/.config/nvim/lua/usr/lsp/settings/vimlatex.vim")
 
--- lua/usr/plugins/init.lua
+++ lua/usr/plugins/init.lua
@@ -53,6 +53,24 @@ return {
 	"neovim/nvim-lspconfig", -- enable LSP
 	"williamboman/nvim-lsp-installer",
 	-- show function signature
+	{
+		"lvimuser/lsp-inlayhints.nvim",
+		event = "LspAttach",
+		branch = "anticonceal",
+		opts = {},
+		init = function()
+			vim.api.nvim_create_autocmd("LspAttach", {
+				group = vim.api.nvim_create_augroup("LspAttach_inlayhints", {}),
+				callback = function(args)
+					if not (args.data and args.data.client_id) then
+						return
+					end
+					local client = vim.lsp.get_client_by_id(args.data.client_id)
+					require("lsp-inlayhints").on_attach(client, args.buf)
+				end,
+			})
+		end,
+	},
 	"ray-x/lsp_signature.nvim",
 	"jose-elias-alvarez/null-ls.nvim",
 	"antoinemadec/FixCursorHold.nvim", -- This is needed to fix lsp doc highlight
