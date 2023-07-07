b78e218df1c7b90fa3ebd103e1fada4cfb562c7a
Filip Lobpreis <f.lobpreis@gmail.com>
2023-07-07 22:40:03 +0200

    dropbar: plugin update
    Signed-off-by: Filip Lobpreis <f.lobpreis@gmail.com>

--- lua/usr/navigation/dropbar.lua
+++ lua/usr/navigation/dropbar.lua
@@ -4,42 +4,12 @@ if not ok then
 	return
 end
 
+local utils = require('dropbar.utils')
 local icons = require("usr.core.icons")
 
 dropbar.setup{
-	general = {
-		---@type boolean|fun(buf: integer, win: integer): boolean
-		enable = function(buf, win)
-			return not vim.api.nvim_win_get_config(win).zindex
-				and vim.bo[buf].buftype == ''
-				and vim.api.nvim_buf_get_name(buf) ~= ''
-				and not vim.wo[win].diff
-		end,
-		update_events = {
-			win = {
-				'CursorMoved',
-				'CursorMovedI',
-				'WinEnter',
-				'WinLeave',
-				'WinResized',
-				'WinScrolled',
-			},
-			buf = {
-				'BufModifiedSet',
-				'FileChangedShellPost',
-				'TextChanged',
-				'TextChangedI',
-			},
-			global = {
-				'DirChanged',
-				'VimResized',
-			}
-
-		},
-	},
 	icons = {
 		kinds = {
-			use_devicons = true,
 			symbols = {
 				Array = icons.kind.Array,
 				Boolean = icons.kind.Boolean,
@@ -105,105 +75,10 @@ dropbar.setup{
 				WhileStatement = icons.kind.While,
 			},
 		},
-		ui = {
-			bar = {
-				separator = icons.ui.Separator,
-				extends = icons.ui.Extends,
-			},
-			menu = {
-				separator = " ",
-				indicator = icons.ui.Indicator,
-			},
-		},
-	},
-	symbol = {
-		preview = {
-			reorient = function(_, range)
-				local invisible = range['end'].line - vim.fn.line('w$') + 1
-				if invisible > 0 then
-					local view = vim.fn.winsaveview()
-					view.topline = view.topline + invisible
-					vim.fn.winrestview(view)
-				end
-			end,
-		},
-		jump = {
-			reorient = function(win, range)
-				local view = vim.fn.winsaveview()
-				local win_height = vim.api.nvim_win_get_height(win)
-				local topline = range.start.line - math.floor(win_height / 4)
-				if
-					topline > view.topline
-					and topline + win_height < vim.fn.line('$')
-				then
-					view.topline = topline
-					vim.fn.winrestview(view)
-				end
-			end,
-		},
-	},
-	bar = {
-		---@type dropbar_source_t[]|fun(buf: integer, win: integer): dropbar_source_t[]
-		sources = function(_, _)
-			local sources = require('dropbar.sources')
-			return {
-				sources.path,
-				{
-					get_symbols = function(buf, win, cursor)
-						if vim.bo[buf].ft == 'markdown' then
-							return sources.markdown.get_symbols(buf, win, cursor)
-						end
-						for _, source in ipairs({
-							sources.lsp,
-							sources.treesitter,
-						}) do
-							local symbols = source.get_symbols(buf, win, cursor)
-							if not vim.tbl_isempty(symbols) then
-								return symbols
-							end
-						end
-						return {}
-					end,
-				},
-			}
-		end,
-		padding = {
-			left = 1,
-			right = 1,
-		},
-		pick = {
-			pivots = 'abcdefghijklmnopqrstuvwxyz',
-		},
-		truncate = true,
 	},
 	menu = {
-		entry = {
-			padding = {
-				left = 1,
-				right = 1,
-			},
-		},
 		---@type table<string, string|function|table<string, string|function>>
 		keymaps = {
-			['<LeftMouse>'] = function()
-				local api = require('dropbar.api')
-				local menu = api.get_current_dropbar_menu()
-				if not menu then
-					return
-				end
-				local mouse = vim.fn.getmousepos()
-				if mouse.winid ~= menu.win then
-					local parent_menu = api.get_dropbar_menu(mouse.winid)
-					if parent_menu and parent_menu.sub_menu then
-						parent_menu.sub_menu:close()
-					end
-					if vim.api.nvim_win_is_valid(mouse.winid) then
-						vim.api.nvim_set_current_win(mouse.winid)
-					end
-					return
-				end
-				menu:click_at({ mouse.line, mouse.column }, nil, 1, 'l')
-			end,
 			['l'] = function()
 				local menu = require('dropbar.api').get_current_dropbar_menu()
 				if not menu then
@@ -218,152 +93,13 @@ dropbar.setup{
 			['q'] = function ()
 				local menu = require('dropbar.api').get_current_dropbar_menu()
 				while menu do
-					menu:close()
-					menu = menu.parent_menu
+					menu:del()
+					menu = menu.prev_menu
 				end
 			end,
 			['h'] = function ()
-				require('dropbar.api').get_current_dropbar_menu():close()
-			end
-		},
-		---@alias dropbar_menu_win_config_opts_t any|fun(menu: dropbar_menu_t):any
-		---@type table<string, dropbar_menu_win_config_opts_t>
-		---@see vim.api.nvim_open_win
-		win_configs = {
-			border = 'none',
-			style = 'minimal',
-			row = function(menu)
-				return menu.parent_menu
-						and menu.parent_menu.clicked_at
-						and menu.parent_menu.clicked_at[1] - vim.fn.line('w0')
-					or 1
+				require('dropbar.api').get_current_dropbar_menu():del()
 			end,
-			col = function(menu)
-				return menu.parent_menu and menu.parent_menu._win_configs.width or 0
-			end,
-			relative = function(menu)
-				return menu.parent_menu and 'win' or 'mouse'
-			end,
-			win = function(menu)
-				return menu.parent_menu and menu.parent_menu.win
-			end,
-			height = function(menu)
-				return math.max(
-					1,
-					math.min(
-						#menu.entries,
-						vim.go.pumheight ~= 0 and vim.go.pumheight
-							or math.ceil(vim.go.lines / 4)
-					)
-				)
-			end,
-			width = function(menu)
-				local min_width = vim.go.pumwidth ~= 0 and vim.go.pumwidth or 8
-				if vim.tbl_isempty(menu.entries) then
-					return min_width
-				end
-				return math.max(
-					min_width,
-					math.max(unpack(vim.tbl_map(function(entry)
-						return entry:displaywidth()
-					end, menu.entries)))
-				)
-			end,
-		},
-	},
-	sources = {
-		path = {
-			---@type string|fun(buf: integer): string
-			relative_to = function(_)
-				return vim.fn.getcwd()
-			end,
-			---Can be used to filter out files or directories
-			---based on their name
-			---@type fun(name: string): boolean
-			filter = function(_)
-				return true
-			end,
-			modified = function(sym)
-				return sym:merge({
-					name = sym.name .. '[+]',
-					icon = ' ',
-					name_hl = 'DiffAdded',
-					icon_hl = 'DiffAdded',
-					-- ...
-				})
-			end
-		},
-		treesitter = {
-			-- Lua pattern used to extract a short name from the node text
-			-- Be aware that the match result must not be nil!
-			name_pattern = string.rep('[#~%w%._%->!]*', 4, '%s*'),
-			-- The order matters! The first match is used as the type
-			-- of the treesitter symbol and used to show the icon
-			-- Types listed below must have corresponding icons
-			-- in the `icons.kinds.symbols` table for the icon to be shown
-			valid_types = {
-				'array',
-				'boolean',
-				'break_statement',
-				'call',
-				'case_statement',
-				'class',
-				'constant',
-				'constructor',
-				'continue_statement',
-				'delete',
-				'do_statement',
-				'enum',
-				'enum_member',
-				'event',
-				'for_statement',
-				'function',
-				'if_statement',
-				'interface',
-				'keyword',
-				'list',
-				'macro',
-				'method',
-				'module',
-				'namespace',
-				'null',
-				'number',
-				'operator',
-				'package',
-				'property',
-				'reference',
-				'repeat',
-				'scope',
-				'specifier',
-				'string',
-				'struct',
-				'switch_statement',
-				'type',
-				'type_parameter',
-				'unit',
-				'value',
-				'variable',
-				'while_statement',
-				'declaration',
-				'field',
-				'identifier',
-				'object',
-				'statement',
-				'text',
-			},
-		},
-		lsp = {
-			request = {
-				-- Times to retry a request before giving up
-				ttl_init = 60,
-				interval = 1000, -- in ms
-			},
-		},
-		markdown = {
-			parse = {
-				-- Number of lines to update when cursor moves out of the parsed range
-				look_ahead = 200,
-			},
 		},
 	},
 }
