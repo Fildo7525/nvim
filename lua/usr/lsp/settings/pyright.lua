b17ed542b43fdb3fecfd19466057f56e07163677
Filip Lobpreis <f.lobpreis@gmail.com>
2023-07-30 11:23:12 +0200

    lsp: pyright setup update
    Signed-off-by: Filip Lobpreis <f.lobpreis@gmail.com>

--- lua/usr/lsp/settings/pyright.lua
+++ lua/usr/lsp/settings/pyright.lua
@@ -1,10 +1,18 @@
 return {
 	settings = {
-
+		cmd = { vim.fn.stdpath("data") .. "/mason/bin/pyright-langserver", "--stdio" },
+		filetypes = { "python" },
+		root_dir = function(fname)
+			return require("lspconfig").util.find_git_ancestor(fname) or vim.fn.getcwd()
+		end,
 		python = {
 			analysis = {
-				typeCheckingMode = "off"
+				typeCheckingMode = "off",
+				autoSearchPaths = true,
+				diagnosticMode = "workspace",
+				useLibraryCodeForTypes = true
 			}
-		}
+		},
+		single_file_support = true,
 	},
 }
