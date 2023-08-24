3b1f7d95763e7e8a8ec79431433397ec7b859a8f
Filip Lobpreis <f.lobpreis@gmail.com>
2023-08-24 19:06:41 +0200

    nvim-tree: api updated
    Signed-off-by: Filip Lobpreis <f.lobpreis@gmail.com>

--- lua/usr/navigation/nvim-tree.lua
+++ lua/usr/navigation/nvim-tree.lua
@@ -2,20 +2,10 @@
 -- each of these are documented in `:help nvim-tree.OPTION_NAME`
 local nvim_tree = require("nvim-tree")
 
+vim.g.loaded_netrw = 1
+vim.g.loaded_netrwPlugin = 1
+
 local config = {
-	disable_netrw = true,
-	hijack_netrw = true,
-	root_dirs = {},
-	prefer_startup_root = true,
-	sync_root_with_cwd = true,
-	respect_buf_cwd = true,
-	open_on_tab = false,
-	hijack_cursor = false,
-	update_cwd = true,
-	-- update_to_buf_dir = {
-	-- 	enable = true,
-	-- 	auto_open = true,
-	-- },
 	diagnostics = {
 		enable = true,
 		icons = {
@@ -25,27 +15,6 @@ local config = {
 			error = "",
 		},
 	},
-	update_focused_file = {
-		enable = true,
-		update_cwd = true,
-		ignore_list = {},
-	},
-	git = {
-		enable = true,
-		ignore = true,
-		timeout = 500,
-	},
-	view = {
-		width = 30,
-		hide_root_folder = false,
-		side = "left",
-		-- auto_resize = true,
-		mappings = {
-			custom_only = false,
-		},
-		number = false,
-		relativenumber = false,
-	},
 	renderer = {
 		icons = {
 			glyphs = {
@@ -53,11 +22,11 @@ local config = {
 				symlink = "",
 				git = {
 					unstaged = "",
-					staged = "S",
+					staged = "",
 					unmerged = "",
 					renamed = "➜",
+					untracked = "",
 					deleted = "",
-					untracked = "U",
 					ignored = "◌",
 				},
 				folder = {
@@ -67,20 +36,9 @@ local config = {
 					empty_open = "",
 					symlink = "",
 				},
-			}
+			},
 		},
 	},
-	-- quit_on_open = 0,
-	-- git_hl = 1,
-	-- disable_window_picker = 0,
-	-- root_folder_modifier = ":t",
-	-- show_icons = {
-	-- 	git = 1,
-	-- 	folders = 1,
-	-- 	files = 1,
-	-- 	folder_arrows = 1,
-	-- 	tree_width = 30,
-	-- },
 }
 
 -- open_on_setup
@@ -105,9 +63,8 @@ local function open_nvim_tree(data)
 	require("nvim-tree.api").tree.open()
 end
 
-local function on_attach(bufnr)
+local function set_keymaps(bufnr)
 	local api = require('nvim-tree.api')
-
 	local opts = function(desc)
 	 return { desc = 'nvim-tree: ' .. desc, buffer = bufnr, noremap = true, silent = true, nowait = true }
 	end
@@ -205,7 +162,10 @@ local function on_attach(bufnr)
 	end, opts('Print Node Path'))
 
 	vim.keymap.set('n', 'Z', api.node.run.system, opts('Run System'))
+end
 
+local function on_attach(bufnr)
+	set_keymaps(bufnr)
 end
 
 
