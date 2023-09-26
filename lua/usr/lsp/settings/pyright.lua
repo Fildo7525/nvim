6427cc69d76493a305660a9602294ae53118f3fb
Filip Lobpreis <f.lobpreis@gmail.com>
2023-09-26 19:49:51 +0200

    lsp: python update
    Signed-off-by: Filip Lobpreis <f.lobpreis@gmail.com>

--- lua/usr/lsp/settings/pyright.lua
+++ lua/usr/lsp/settings/pyright.lua
@@ -1,21 +1,35 @@
+local util = require 'lspconfig.util'
+
+--[[ local root_files = { ]]
+--[[ 	'main.py', ]]
+--[[ 	'pyproject.toml', ]]
+--[[ 	'setup.py', ]]
+--[[ 	'setup.cfg', ]]
+--[[ 	'requirements.txt', ]]
+--[[ 	'Pipfile', ]]
+--[[ 	'pyrightconfig.json', ]]
+--[[ 	'run', ]]
+--[[ 	'compile', ]]
+--[[ } ]]
+
 return {
 	settings = {
 		cmd = { vim.fn.stdpath("data") .. "/mason/bin/pyright-langserver", "--stdio" },
 		filetypes = { "python" },
-		root_dir = function(fname)
-			return require("lspconfig").util.find_git_ancestor(fname) or vim.fn.getcwd()
-		end,
+		--[[ root_dir = function(fname) ]]
+		--[[ 	return util.root_pattern(unpack(root_files))(fname) or util.find_git_ancestor(fname) ]]
+		--[[ end, ]]
 		python = {
-			disableLanguageServices = false,
-			disableOrganizeImports = false,
-			openFilesOnly = false,
-			analysis = {
-				autoImportCompletions = true,
-				autoSearchPaths = true,
-				diagnosticMode = "workspace",
-				typeCheckingMode = "basic", -- off, basic, strict
-				useLibraryCodeForTypes = true,
-			}
+				disableLanguageServices = false,
+				disableOrganizeImports = false,
+				openFilesOnly = false,
+				analysis = {
+					autoImportCompletions = true,
+					autoSearchPaths = true,
+					diagnosticMode = "workspace",
+					typeCheckingMode = "basic", -- off, basic, strict
+					useLibraryCodeForTypes = true,
+				}
 		},
 		single_file_support = true,
 	},
