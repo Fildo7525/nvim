460fc8035f947dc6d23727c2ee129c4ac4f9c287
Filip Lobpreis <f.lobpreis@gmail.com>
2024-02-02 14:56:44 +0100

    treesitter: disable with larg files
    Signed-off-by: Filip Lobpreis <f.lobpreis@gmail.com>

--- lua/usr/ts/treesitter.lua
+++ lua/usr/ts/treesitter.lua
@@ -26,7 +26,13 @@ treesitter.setup({
 		enable = true,
 
 		-- list of language that will be disabled
-		disable = { "" },
+		disable = function(lang, buf)
+			local max_filesize = 100 * 1024 -- 100 KB
+			local ok, stats = pcall(vim.loop.fs_stat, vim.api.nvim_buf_get_name(buf))
+			if ok and stats and stats.size > max_filesize then
+				return true
+			end
+		end,
 
 		-- Setting this to true will run `:h syntax` and tree-sitter at the same time.
 		-- Set this to `true` if you depend on 'syntax' being enabled (like for indentation).
