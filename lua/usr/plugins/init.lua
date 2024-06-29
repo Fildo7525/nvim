621b807e1de0b03c59d9c2fc25783d849427dda5
Filip Lobpreis <f.lobpreis@gmail.com>
2024-06-29 12:31:55 +0200

    luasnip: set the release tag
    Signed-off-by: Filip Lobpreis <f.lobpreis@gmail.com>

--- lua/usr/plugins/init.lua
+++ lua/usr/plugins/init.lua
@@ -34,7 +34,13 @@ return {
 	"karb94/neoscroll.nvim",
 
 	-- snippets
-	"L3MON4D3/LuaSnip", --snippet engine
+	{
+		"L3MON4D3/LuaSnip",
+		-- follow latest release.
+		version = "v2.*", -- Replace <CurrentMajor> by the latest released major (first number of latest release)
+		-- install jsregexp (optional!).
+		build = "make install_jsregexp",
+	},
 	"rafamadriz/friendly-snippets", -- a bunch of snippets to use
 
 	-- cmp plugins
