f28675421d5b39db6e1655701f84fcc75915e831
Filip Lobpreis <f.lobpreis@gmail.com>
2024-08-29 12:50:09 +0200

    lsp: revert 5216de7b (lsp: pyright remove unused code)
    This causes the pyright to stop at error 'File or directory "/<default
    workspace root>" does not exist.'
    
    Signed-off-by: Filip Lobpreis <f.lobpreis@gmail.com>

--- lua/usr/lsp/settings/pyright.lua
+++ lua/usr/lsp/settings/pyright.lua
@@ -1,3 +1,18 @@
+local util = require 'lspconfig.util'
+
+local root_files = {
+	'.venv',
+	'main.py',
+	'pyproject.toml',
+	'setup.py',
+	'setup.cfg',
+	'requirements.txt',
+	'Pipfile',
+	'pyrightconfig.json',
+	'run',
+	'compile',
+}
+
 local function getPipenvPath()
 	local env = os.getenv('VIRTUAL_ENV')
 	if env == nil or #env == 0 then
@@ -10,6 +25,9 @@ return {
 	settings = {
 		cmd = { vim.fn.stdpath("data") .. "/mason/bin/pyright-langserver", "--stdio" },
 		filetypes = { "python" },
+		root_dir = function(fname)
+			return util.root_pattern(unpack(root_files))(fname) or util.find_git_ancestor(fname)
+		end,
 		python = {
 			disableLanguageServices = false,
 			disableOrganizeImports = false,
