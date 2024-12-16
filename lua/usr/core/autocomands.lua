3e083bf75c59ae8ecb26474769564e430211a0de
Filip Lobpreis <f.lobpreis@gmail.com>
2024-12-16 11:52:53 +0100

    autocomands: extended autocomands
    * Trim trailing whitespaces on buffer save
    * Save opened files to .cache to be reopened later
    
    Signed-off-by: Filip Lobpreis <f.lobpreis@gmail.com>

--- lua/usr/core/autocomands.lua
+++ lua/usr/core/autocomands.lua
@@ -12,3 +12,35 @@ vim.api.nvim_create_autocmd({ "BufRead", "BufWritePost", "BufNewFile" }, {
 	end,
 	group = matlab_ft_id,
 })
+
+local at_save = vim.api.nvim_create_augroup("Save", {
+	clear = true,
+})
+
+-- Trim trailing whitespace on save
+vim.api.nvim_create_autocmd({ "BufWrite" }, {
+	pattern = { "*" },
+	callback = function()
+		-- Trim trailing whitespace
+		if vim.bo.modifiable then
+			vim.cmd("%s/\\s\\+$//e")
+			vim.cmd.write()
+		end
+	end,
+	group = at_save,
+})
+
+-- Save opened files
+local at_exit = vim.api.nvim_create_augroup("Exit", {
+	clear = true,
+})
+
+vim.api.nvim_create_autocmd({ "ExitPre" }, {
+	pattern = { "*" },
+	callback = function()
+		-- Trim trailing whitespace
+		require('revolver').SaveOpenedFiles()
+	end,
+	group = at_exit,
+})
+
