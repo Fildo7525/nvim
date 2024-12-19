0ac38aea4402d63c571634af901c014cb0b9706b
Filip Lobpreis <f.lobpreis@gmail.com>
2024-12-19 21:56:51 +0100

    core: no need for multiple colorschemes
    Signed-off-by: Filip Lobpreis <f.lobpreis@gmail.com>

--- lua/usr/core/colorscheme/init.lua
+++
@@ -1,9 +0,0 @@
---[[ COLORSCHEME = require("usr.core.colorscheme.configs.tokyo") ]]
-COLORSCHEME = require("usr.core.colorscheme.configs.cattpucin")
-
-local status_ok, _ = pcall(vim.cmd, "colorscheme " .. COLORSCHEME)
-if not status_ok then
-	vim.notify("colorscheme " .. COLORSCHEME .. " not found!", vim.log.levels.ERROR)
-	return
-end
-
