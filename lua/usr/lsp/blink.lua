eabfe46d19221fe2892c49f0febbc7ea6de065e5
Filip Lobpreis <f.lobpreis@gmail.com>
2025-02-21 18:09:55 +0100

    lsp: blink add support for pretty_hover
    Signed-off-by: Filip Lobpreis <f.lobpreis@gmail.com>

--- lua/usr/lsp/blink.lua
+++ lua/usr/lsp/blink.lua
@@ -4,6 +4,15 @@ return {
 			auto_show = true,
 			auto_show_delay_ms = 500,
 			treesitter_highlighting = true,
+			draw = function(opts)
+				local out = {}
+				if opts.item and opts.item.documentation then
+					out = require("pretty_hover.parser").parse(opts.item.documentation.value)
+					opts.item.documentation.value = out:string()
+				end
+
+				opts.default_implementation(opts)
+			end,
 			window = {
 				border = "rounded",
 			},
