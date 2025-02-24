a83260d458cd39582a0840a417f5d72abc478d3f
Filip Lobpreis <f.lobpreis@gmail.com>
2025-02-24 10:09:43 +0100

    lazy: test
    Signed-off-by: Filip Lobpreis <f.lobpreis@gmail.com>

--- lua/usr/plugins/init.lua
+++ lua/usr/plugins/init.lua
@@ -104,7 +104,7 @@ return {
 		dependencies = 'rafamadriz/friendly-snippets',
 
 		-- use a release tag to download pre-built binaries
-		version = '*',
+		--[[ version = '*', ]]
 		-- AND/OR build from source, requires nightly: https://rust-lang.github.io/rustup/concepts/channels.html#working-with-nightly-rust
 		-- build = 'cargo build --release',
 		-- If you use nix, you can build from source using latest nightly rust with:
