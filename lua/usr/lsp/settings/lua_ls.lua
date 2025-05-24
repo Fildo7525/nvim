5b3884a87b1467ae5ddd7950fc3c259b59a0adb8
Filip Lobpreis <f.lobpreis@gmail.com>
2025-05-25 01:07:07 +0200

    lsp: lua_ls: udpate based on the default config
    Signed-off-by: Filip Lobpreis <f.lobpreis@gmail.com>

--- lua/usr/lsp/settings/lua_ls.lua
+++ lua/usr/lsp/settings/lua_ls.lua
@@ -1,6 +1,10 @@
 return {
+	cmd = { "lua-language-server" },
+	filetypes = { "lua" },
+	root_markers = { ".luarc.json", ".luarc.jsonc" },
 	settings = {
 		Lua = {
+			hint = { enable = true, },
 			diagnostics = {
 				globals = { "vim" },
 			},
