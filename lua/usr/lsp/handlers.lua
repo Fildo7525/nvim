44804a66b829791a5308eeeb87a032ab0a0368ce
Filip Lobpreis <f.lobpreis@gmail.com>
2025-05-25 00:46:19 +0200

    lsp: border is set by vim.o.winborder
    There is no need to setup the border anymore for the requests
    specifically.
    
    Signed-off-by: Filip Lobpreis <f.lobpreis@gmail.com>

--- lua/usr/lsp/handlers.lua
+++ lua/usr/lsp/handlers.lua
@@ -35,14 +35,6 @@ function M.setup()
 	}
 
 	vim.diagnostic.config(config)
-
-	vim.lsp.handlers["textDocument/hover"] = vim.lsp.with(vim.lsp.handlers.hover, {
-		border = "rounded",
-	})
-
-	vim.lsp.handlers["textDocument/signatureHelp"] = vim.lsp.with(vim.lsp.handlers.signature_help, {
-		border = "rounded",
-	})
 end
 
 local function lsp_highlight_document(client)
