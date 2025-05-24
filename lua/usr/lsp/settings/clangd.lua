f7b7efdba0a8b2295da1a50c4b81c45b9f86ae06
Filip Lobpreis <f.lobpreis@gmail.com>
2025-05-25 01:07:42 +0200

    lsp: clangd: remove flags the lsp does not use
    The logs suggest that these flags are ignored at the startup.
    
    Signed-off-by: Filip Lobpreis <f.lobpreis@gmail.com>

--- lua/usr/lsp/settings/clangd.lua
+++ lua/usr/lsp/settings/clangd.lua
@@ -33,15 +33,11 @@ return {
 		"--compile_args_from=filesystem", -- lsp-> does not come from compie_commands.json
 		"--completion-parse=always",
 		"--completion-style=bundled",
-		"--cross-file-rename",
 		"--debug-origin",
 		"--enable-config", -- clangd 11+ supports reading from .clangd configuration file
 		"--fallback-style=Qt",
-		"--folding-ranges",
-		"--function-arg-placeholders",
 		"--header-insertion=iwyu",
 		"--pch-storage=memory", -- could also be disk
-		"--suggest-missing-includes",
 		"-j=4",		-- number of workers
 		-- "--resource-dir="
 		"--log=error",
