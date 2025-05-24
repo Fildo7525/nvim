cf26db4ab1a96f40b2e6084452b3b9101aacb743
Filip Lobpreis <f.lobpreis@gmail.com>
2025-05-24 15:10:00 +0200

    illuminate: keymaps
    Signed-off-by: Filip Lobpreis <f.lobpreis@gmail.com>

--- lua/usr/illuminate.lua
+++ lua/usr/illuminate.lua
@@ -1,6 +1,4 @@
--- vim.g.Illuminate_delay = 0
+vim.g.Illuminate_ftblacklist = { 'alpha', 'NvimTree', 'neo-tree', 'dashboard', 'lazy', 'mason' }
 -- vim.g.Illuminate_highlightUnderCursor = 0
-vim.g.Illuminate_ftblacklist = {'alpha', 'NvimTree'}
--- vim.g.Illuminate_highlightUnderCursor = 0
-vim.api.nvim_set_keymap('n', '.', '<cmd>lua require"illuminate".next_reference{wrap=true}<cr>', {noremap=true})
-vim.api.nvim_set_keymap('n', ',', '<cmd>lua require"illuminate".next_reference{reverse=true,wrap=true}<cr>', {noremap=true})
+vim.api.nvim_set_keymap('n', '.', '<cmd>lua require"illuminate".next_reference({wrap=true})<cr>', {noremap=true})
+vim.api.nvim_set_keymap('n', ',', '<cmd>lua require"illuminate".next_reference({reverse=true, wrap=true})<cr>', {noremap=true})
