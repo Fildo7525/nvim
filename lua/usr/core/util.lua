dc5ea21c716d61a0703943214cf1cb0dcfe54c39
Filip Lobpreis <f.lobpreis@gmail.com>
2025-11-09 23:09:52 +0100

    cp: core: util: fix indentation
    Signed-off-by: Filip Lobpreis <f.lobpreis@gmail.com>

--- lua/usr/core/util.lua
+++ lua/usr/core/util.lua
@@ -27,36 +27,34 @@ end
 
 function M.register_options(options)
 	for k, v in pairs(options) do
-	if type(v) ~= "table" then
-		vim.opt[k] = v
-		goto continue
-	end
-
-	if #v % 2 == 1 then
-		vim.notify("Table must have an even number of elements. Option " .. k, vim.log.levels.ERROR)
-		goto continue
-	end
-
-	for i=1, #v/2 do
-		if v[i*2-1] == M.opt.append then
-			vim.opt[k]:append(v[i*2])
-
-		elseif v[i*2-1] == M.opt.remove then
-			vim.opt[k]:remove(v[i*2])
-
-		elseif v[i*2-1] == M.opt.prepend then
-			vim.opt[k]:prepend(v[i*2])
-
-		else
-			error("Invalid option")
-
+		if type(v) ~= "table" then
+			vim.opt[k] = v
+			goto continue
 		end
+
+		if #v % 2 == 1 then
+			vim.notify("Table must have an even number of elements. Option " .. k, vim.log.levels.ERROR)
+			goto continue
+		end
+
+		for i=1, #v/2 do
+			if v[i*2-1] == M.opt.append then
+				vim.opt[k]:append(v[i*2])
+
+			elseif v[i*2-1] == M.opt.remove then
+				vim.opt[k]:remove(v[i*2])
+
+			elseif v[i*2-1] == M.opt.prepend then
+				vim.opt[k]:prepend(v[i*2])
+
+			else
+				error("Invalid option")
+
+			end
+		end
+
+		::continue::
 	end
-
-	::continue::
-end
-
-
 end
 
 return M
