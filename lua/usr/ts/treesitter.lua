43278d41453f171a6e037f176297076e4a91d27d
Filip Lobpreis <f.lobpreis@gmail.com>
2025-11-18 20:47:49 +0100

    ts: extend ensure installed
    Signed-off-by: Filip Lobpreis <f.lobpreis@gmail.com>

--- lua/usr/ts/treesitter.lua
+++ lua/usr/ts/treesitter.lua
@@ -3,7 +3,13 @@ local treesitter = require("nvim-treesitter.configs")
 treesitter.setup({
 	-- One of "all", "maintained" (parsers with maintainers), or a list of languages
 	-- ensure_installed = "maintained",
-	ensure_installed = { "bash", "bibtex", "c", "c_sharp", "cmake", "comment", "cpp", "desktop", "diff", "dockerfile", "doxygen", "git_config", "git_rebase", "gitattributes", "gitcommit", "gitignore", "javascript", "json", "lua", "luadoc", "markdown", "markdown_inline", "python", "ssh_config", "vim", "vimdoc", "yaml", "thrift", "toml", "yaml"},
+	ensure_installed = {
+		"bash", "bibtex", "c", "c_sharp", "cmake", "comment", "cpp", "desktop", "diff",
+		"dockerfile", "doxygen", "git_config", "git_rebase", "gitattributes", "gitcommit",
+		"gitignore", "html", "javascript", "json", "latex", "lua", "luadoc", "markdown",
+		"markdown_inline", "python", "rust", "ssh_config", "thrift", "toml", "vim",
+		"vimdoc", "xml", "yaml", "yaml",
+	},
 
 	-- Install languages synchronously (only applied to `ensure_installed`)
 	sync_install = false,
