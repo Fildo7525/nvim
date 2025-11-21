6e0e1878ae3cb72ed60614bffec5e02ffe77894a
Filip Lobpreis <f.lobpreis@gmail.com>
2025-11-21 15:18:04 +0100

    gitsigns: config update
    Signed-off-by: Filip Lobpreis <f.lobpreis@gmail.com>

--- lua/usr/gitsigns.lua
+++ lua/usr/gitsigns.lua
@@ -2,40 +2,48 @@ local gitsigns = require("gitsigns")
 local icons = require("usr.core.icons")
 
 gitsigns.setup {
-	debug_mode = false,
 	signs = {
 		add = { text = icons.git.BoldBar },
 		change = { text = icons.git.BoldBar },
 		delete = { text = icons.git.Deleted },
-		topdelete = { text = icons.git.Deleted },
-		changedelete = { text = icons.git.BoldBar },
+		topdelete = { text = icons.git.TopDeleted },
+		changedelete = { text = icons.git.ChagneDeleted },
 		untracked = { text = icons.git.Untracked },
 	},
+	signs_staged = {
+		add					= { text = icons.git.BoldBar },
+		change			 = { text = icons.git.BoldBar },
+		delete			 = { text = icons.git.Deleted },
+		topdelete		= { text = icons.git.TopDeleted },
+		changedelete = { text = icons.git.ChagneDeleted },
+		untracked		= { text = icons.git.Untracked },
+	},
+	signs_staged_enable = true,
 	signcolumn = true, -- Toggle with `:Gitsigns toggle_signs`
 	numhl = false, -- Toggle with `:Gitsigns toggle_numhl`
 	linehl = false, -- Toggle with `:Gitsigns toggle_linehl`
 	word_diff = false, -- Toggle with `:Gitsigns toggle_word_diff`
 	watch_gitdir = {
-		interval = 1000,
 		follow_files = true,
 	},
 	auto_attach = true,
-	attach_to_untracked = true,
+	attach_to_untracked = false,
 	current_line_blame = false, -- Toggle with `:Gitsigns toggle_current_line_blame`
 	current_line_blame_opts = {
 		virt_text = true,
-		virt_text_pos = "right_align", -- 'eol' | 'overlay' | 'right_align'
+		virt_text_pos = 'eol', -- 'eol' | 'overlay' | 'right_align'
 		delay = 1000,
 		ignore_whitespace = false,
+		virt_text_priority = 100,
+		use_focus = true,
 	},
 	current_line_blame_formatter = '<author>, <author_time:%Y-%m-%d> - <summary>',
 	sign_priority = 6,
 	update_debounce = 100,
 	status_formatter = nil, -- Use default
-	max_file_length = 40000,
+	max_file_length = 40000, -- Disable if file is longer than this (in lines)
 	preview_config = {
 		-- Options passed to nvim_open_win
-		border = "rounded",
 		style = "minimal",
 		relative = "cursor",
 		row = 0,
