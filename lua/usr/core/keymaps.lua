5533d867d18e5b81c96252026e8f22b73bd40835
Filip Lobpreis <f.lobpreis@gmail.com>
2025-11-30 13:18:08 +0100

    keymaps: clang_reloader do not use telescope
    Signed-off-by: Filip Lobpreis <f.lobpreis@gmail.com>

--- lua/usr/core/keymaps.lua
+++ lua/usr/core/keymaps.lua
@@ -76,7 +76,7 @@ keymap("n", "<leader>fs", require("telescope.builtin").grep_string, opts)
 keymap("n", "<leader>of", require("telescope.builtin").oldfiles, opts)
 keymap("n", "<leader>re", require("telescope.builtin").resume, opts)
 keymap("n", "<leader>pr", require("telescope").extensions.projects.projects, opts)
-keymap('n', "<leader>cc", require("telescope").extensions.clang_reloader.clang_reloader, opts)
+keymap('n', "<leader>cc", require("clang_reloader").reload, opts)
 
 -- telescope git commands
 keymap("n", "<leader>gb", require("telescope.builtin").git_branches, opts)
