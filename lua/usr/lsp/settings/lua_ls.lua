2d3699c9b7de10d608eb3fb66b1465b1142a9919
Filip Lobpreis <f.lobpreis@gmail.com>
2025-12-02 21:39:30 +0100

    lsp: lua_ls: add git to root markers
    Signed-off-by: Filip Lobpreis <f.lobpreis@gmail.com>

--- lua/usr/lsp/settings/lua_ls.lua
+++ lua/usr/lsp/settings/lua_ls.lua
@@ -1,7 +1,7 @@
 return {
 	cmd = { "lua-language-server" },
 	filetypes = { "lua" },
-	root_markers = { ".luarc.json", ".luarc.jsonc" },
+	root_markers = { ".luarc.json", ".luarc.jsonc", ".git", "stylua.toml" },
 	settings = {
 		Lua = {
 			hint = { enable = true, },
