eb1577c3577508748d4d2c5d27e1d8558d770aea
Filip Lobpreis <f.lobpreis@gmail.com>
2026-04-14 00:48:17 +0200

    lazy: use treesitter on main
    Signed-off-by: Filip Lobpreis <f.lobpreis@gmail.com>

--- lua/usr/plugins/init.lua
+++ lua/usr/plugins/init.lua
@@ -201,7 +201,6 @@ return {
 	-- TREESITTER
 	{
 		"nvim-treesitter/nvim-treesitter",
-		tag = "v0.10.0",
 		lazy = false,
 		build = ":TSUpdate"
 	},
--- lua/usr/ts/treesitter.lua
+++ lua/usr/ts/treesitter.lua
@@ -1,40 +1,11 @@
-local treesitter = require("nvim-treesitter.configs")
+local treesitter = require("nvim-treesitter")
 
-treesitter.setup({
-	-- One of "all", "maintained" (parsers with maintainers), or a list of languages
-	-- ensure_installed = "maintained",
-	ensure_installed = {
-		"bash", "bibtex", "c", "c_sharp", "cmake", "comment", "cpp", "desktop", "diff",
-		"dockerfile", "doxygen", "git_config", "git_rebase", "gitattributes", "gitcommit",
-		"gitignore", "html", "javascript", "json", "lua", "luadoc", "markdown",
-		"markdown_inline", "python", "rust", "ssh_config", "thrift", "toml", "vim",
-		"vimdoc", "xml", "yaml", "yaml",
-	},
+local ensure_installed = {
+	"bash", "bibtex", "c", "c_sharp", "cmake", "comment", "cpp", "desktop", "diff",
+	"dockerfile", "doxygen", "git_config", "git_rebase", "gitattributes", "gitcommit",
+	"gitignore", "html", "javascript", "json", "lua", "luadoc", "markdown",
+	"markdown_inline", "python", "rust", "ssh_config", "thrift", "toml", "vim",
+	"vimdoc", "xml", "yaml", "yaml",
+},
 
-	-- Install languages synchronously (only applied to `ensure_installed`)
-	sync_install = false,
-
-	highlight = {
-		-- `false` will disable the whole extension
-		enable = true,
-
-		-- list of language that will be disabled
-		disable = function(lang, buf)
-			local max_filesize = 100 * 1024 -- 100 KB
-			local ok, stats = pcall(vim.loop.fs_stat, vim.api.nvim_buf_get_name(buf))
-			if ok and stats and stats.size > max_filesize then
-				return true
-			end
-
-			return false
-		end,
-
-		-- Setting this to true will run `:h syntax` and tree-sitter at the same time.
-		-- Set this to `true` if you depend on 'syntax' being enabled (like for indentation).
-		-- Using this option may slow down your editor, and you may see some duplicate highlights.
-		-- Instead of true it can also be a list of languages
-		additional_vim_regex_highlighting = false,
-	},
-
-	indent = { enable = true, disable = { "yaml" } },
-})
+treesitter.install(ensure_installed):wait(30000)
